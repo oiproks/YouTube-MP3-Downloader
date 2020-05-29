39236adc41c544c17553aafe7a5fe85c916f8191
Ezio <frascaarelli.a@elettric80.it>
2020-05-29 17:16:36 +0200

    Done

---
+++ YT2MP3/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace YT2MP3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.lblUrl = new System.Windows.Forms.Label();
+            this.lblDestination = new System.Windows.Forms.Label();
+            this.flpDestination = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.progBar = new System.Windows.Forms.ProgressBar();
+            this.lblUpdate = new System.Windows.Forms.Label();
+            this.flpDestination.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(47, 6);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(191, 20);
+            this.txtURL.TabIndex = 2;
+            this.txtURL.TextChanged += new System.EventHandler(this.txtURL_TextChanged);
+            // 
+            // lblUrl
+            // 
+            this.lblUrl.AutoSize = true;
+            this.lblUrl.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUrl.Location = new System.Drawing.Point(12, 9);
+            this.lblUrl.Name = "lblUrl";
+            this.lblUrl.Size = new System.Drawing.Size(29, 13);
+            this.lblUrl.TabIndex = 3;
+            this.lblUrl.Text = "URL";
+            // 
+            // lblDestination
+            // 
+            this.lblDestination.AutoSize = true;
+            this.lblDestination.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDestination.Location = new System.Drawing.Point(41, 15);
+            this.lblDestination.Margin = new System.Windows.Forms.Padding(0, 15, 0, 0);
+            this.lblDestination.Name = "lblDestination";
+            this.lblDestination.Size = new System.Drawing.Size(120, 13);
+            this.lblDestination.TabIndex = 5;
+            this.lblDestination.Text = "Select destination folder";
+            this.lblDestination.MouseClick += new System.Windows.Forms.MouseEventHandler(this.flpDestination_MouseClick);
+            // 
+            // flpDestination
+            // 
+            this.flpDestination.Controls.Add(this.btnSelectFolder);
+            this.flpDestination.Controls.Add(this.lblDestination);
+            this.flpDestination.Location = new System.Drawing.Point(12, 64);
+            this.flpDestination.Name = "flpDestination";
+            this.flpDestination.Size = new System.Drawing.Size(186, 40);
+            this.flpDestination.TabIndex = 6;
+            this.flpDestination.MouseClick += new System.Windows.Forms.MouseEventHandler(this.flpDestination_MouseClick);
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.BackColor = System.Drawing.Color.Transparent;
+            this.btnSelectFolder.BackgroundImage = global::YT2MP3.Properties.Resources.folder;
+            this.btnSelectFolder.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnSelectFolder.FlatAppearance.BorderSize = 0;
+            this.btnSelectFolder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSelectFolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSelectFolder.Location = new System.Drawing.Point(3, 3);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(35, 35);
+            this.btnSelectFolder.TabIndex = 4;
+            this.btnSelectFolder.TabStop = false;
+            this.btnSelectFolder.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnSelectFolder.UseVisualStyleBackColor = false;
+            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.BackColor = System.Drawing.Color.Transparent;
+            this.btnConvert.BackgroundImage = global::YT2MP3.Properties.Resources.play_disabled;
+            this.btnConvert.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnConvert.Enabled = false;
+            this.btnConvert.FlatAppearance.BorderSize = 0;
+            this.btnConvert.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConvert.Location = new System.Drawing.Point(203, 68);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(35, 35);
+            this.btnConvert.TabIndex = 0;
+            this.btnConvert.TabStop = false;
+            this.btnConvert.UseVisualStyleBackColor = false;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // progBar
+            // 
+            this.progBar.Location = new System.Drawing.Point(9, 38);
+            this.progBar.Margin = new System.Windows.Forms.Padding(0);
+            this.progBar.Name = "progBar";
+            this.progBar.Size = new System.Drawing.Size(232, 23);
+            this.progBar.TabIndex = 8;
+            this.progBar.Visible = false;
+            // 
+            // lblUpdate
+            // 
+            this.lblUpdate.BackColor = System.Drawing.Color.Transparent;
+            this.lblUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUpdate.Location = new System.Drawing.Point(9, 38);
+            this.lblUpdate.Margin = new System.Windows.Forms.Padding(0);
+            this.lblUpdate.Name = "lblUpdate";
+            this.lblUpdate.Size = new System.Drawing.Size(229, 22);
+            this.lblUpdate.TabIndex = 9;
+            this.lblUpdate.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblUpdate.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(250, 111);
+            this.Controls.Add(this.lblUpdate);
+            this.Controls.Add(this.progBar);
+            this.Controls.Add(this.flpDestination);
+            this.Controls.Add(this.lblUrl);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.btnConvert);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "YT 2 MP3";
+            this.Shown += new System.EventHandler(this.Form1_Load);
+            this.flpDestination.ResumeLayout(false);
+            this.flpDestination.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Label lblUrl;
+        private System.Windows.Forms.Button btnSelectFolder;
+        private System.Windows.Forms.Label lblDestination;
+        private System.Windows.Forms.FlowLayoutPanel flpDestination;
+        private System.Windows.Forms.ProgressBar progBar;
+        private System.Windows.Forms.Label lblUpdate;
+    }
+}
+
---
+++ YT2MP3/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using YT2MP3.Properties;
+
+namespace YT2MP3
+{
+    public partial class Form1 : Form
+    {
+        string destinationPath;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckUpdate();
+
+            txtURL.Focus();
+        }
+
+        private async void CheckUpdate()
+        {
+            try
+            {
+                string exeUrl = string.Empty;
+                string installedVersion = ConfigurationManager.AppSettings["version"];
+
+                HtmlWeb hw = new HtmlWeb();
+                HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+                doc = hw.Load("https://ytdl-org.github.io/youtube-dl/download.html");
+                foreach (HtmlNode link in doc.DocumentNode.SelectNodes("//a[@href]"))
+                {
+                    // Get the exe Url
+                    if (link.InnerText.Equals("Windows exe"))
+                    {
+                        exeUrl = link.GetAttributeValue("href", string.Empty);
+
+                        continue;
+                    }
+
+                    // Get the latest version date
+                    if (link.InnerText.Contains("2020.") || link.InnerText.Contains("2021."))
+                    {
+                        if (string.IsNullOrEmpty(installedVersion) || !installedVersion.Equals(link.InnerText))
+                        {
+                            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                            config.AppSettings.Settings["version"].Value = link.InnerText;
+                            config.Save(ConfigurationSaveMode.Modified);
+                            ConfigurationManager.RefreshSection("appSettings");
+
+                            if (!string.IsNullOrEmpty(exeUrl)) {
+                                using (WebClient wc = new WebClient())
+                                {
+                                    lblUpdate.Visible = false;
+                                    progBar.Visible = true;
+                                    wc.DownloadProgressChanged += wc_DownloadProgressChanged;
+                                    wc.DownloadFileCompleted += new AsyncCompletedEventHandler(wc_DownloadDataCompleted);
+                                    await wc.DownloadFileTaskAsync(
+                                        new Uri(exeUrl),
+                                        Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Embedded", "youtube-dl.exe"));
+                                    
+                                }
+                            }
+                        }
+                        break;
+                    }
+
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                this.Invoke(new Action(() =>
+                {
+                    MessageBox.Show(
+                    this,
+                    ex.Message,
+                    "ERROR",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Exclamation);
+                }));
+            }
+        }
+
+        void wc_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progBar.Value = e.ProgressPercentage;
+        }
+
+        void wc_DownloadDataCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            progBar.Visible = false;
+            lblUpdate.Visible = true;
+            lblUpdate.Text = "The program has been updated.";
+        }
+
+        private void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.InitialDirectory = "C:\\Users";
+            dialog.IsFolderPicker = true;
+            dialog.Title = "Select destination folder";
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                destinationPath = dialog.FileName;
+                string folder = destinationPath.Substring(destinationPath.LastIndexOf("\\") + 1);
+                lblDestination.Text = folder;
+
+                destinationPath = Path.Combine(destinationPath, "%(title)s.%(ext)s");
+
+                if (!string.IsNullOrEmpty(txtURL.Text.ToString()))
+                {
+                    btnConvert.Enabled = true;
+                    btnConvert.BackgroundImage = Resources.play;
+                }
+            }
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            lblUpdate.Text = "Converting and downloading...";
+            string url = txtURL.Text.ToString();
+
+            txtURL.Text = string.Empty;
+            btnConvert.Enabled = false;
+            btnConvert.BackgroundImage = Resources.play_disabled;
+            flpDestination.Enabled = false;
+            
+            string executablePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Embedded", "youtube-dl.exe");
+
+            ProcessStartInfo procStartInfo = new ProcessStartInfo("cmd", "/c " + string.Format("{0} --extract-audio --audio-format mp3 --audio-quality 0 -o \"{1}\" {2}", executablePath, destinationPath, url));
+
+            procStartInfo.RedirectStandardOutput = true;
+            procStartInfo.UseShellExecute = false;
+            procStartInfo.CreateNoWindow = true;
+
+            using (Process process = new Process())
+            {
+                process.StartInfo = procStartInfo;
+                process.Start();
+
+                process.WaitForExit();
+
+                string result = process.StandardOutput.ReadToEnd();
+                Console.WriteLine(result);
+            }
+
+            lblUpdate.Text = "Conversion complete. File downloaded.";
+            flpDestination.Enabled = true;
+        }
+
+        private void flpDestination_MouseClick(object sender, MouseEventArgs e)
+        {
+            lblUpdate.Text = string.Empty;
+            btnSelectFolder_Click(sender, e);
+        }
+
+        private void txtURL_TextChanged(object sender, EventArgs e)
+        {
+            lblUpdate.Text = string.Empty;
+
+            if (!string.IsNullOrEmpty(txtURL.Text.ToString()) && !string.IsNullOrEmpty(destinationPath))
+            {
+                btnConvert.Enabled = true;
+                btnConvert.BackgroundImage = Resources.play;
+            }
+        }
+    }
+}
